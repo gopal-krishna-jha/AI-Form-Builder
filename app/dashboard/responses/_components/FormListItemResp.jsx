53216308bd578c4934a3e5d82d883a76ce644763
gopal-krishna-jha <gopalkrishnajha06@gmail.com>
2025-03-08 10:06:58 +0530

    2nd commit

--- app/dashboard/responses/_components/FormListItemResp.jsx
+++ app/dashboard/responses/_components/FormListItemResp.jsx
@@ -47,7 +47,7 @@ function FormListItemResp({jsonForm,formRecord}) {
         <h2 className='text-sm text-gray-500'>{jsonForm?.formHeading}</h2>
         <hr className='my-4'></hr>
         <div className='flex justify-between items-center'>
-            <h2 className='text-sm'><strong>69</strong> Responses</h2>
+            <h2 className='text-sm'><strong></strong> Responses</h2>
             <Button className="" size="sm"
             onClick={()=>ExportData()}
             disabled={loading}
